a42aa7e562cb87b9eb8fbc9619ff338f059b1186
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-19 18:26:27 +0330

    used quotable.io instead

--- js/random_quote.js
+++ js/random_quote.js
@@ -1,14 +1,22 @@
-async function getRandomQuote() {
-    try {
-        const response = await fetch('https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en');
-        const quote = await response.json();
-        return quote.quoteText;
-    } catch (error) {
-        console.error('Error getting the quote: ', error)
-        return 'Failed to get a quote, Try again later or report the issue.'
+// DOM elements
+const button = document.getElementById("quote-btn");
+const quote = document.getElementById("quote-txt");
+
+// const cite = document.querySelector("blockquote cite");
+
+async function updateQuote() {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    if (response.ok) {
+        // Update DOM elements
+        quote.textContent = data.content;
+        // cite.textContent = data.author;
+    } else {
+        quote.textContent = "An error occured";
+        console.log(data);
     }
 }
 
-document.getElementById('quote-btn').addEventListener('click', async () => {
-    document.getElementById('quote-txt').textContent = await getRandomQuote();
-});
+// Attach an event listener to the `button`
+button.addEventListener("click", updateQuote);
