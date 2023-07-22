dcaf84ce4443b73100f3a436e4fce84fb51f8905
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-23 00:43:05 +0330

    click spam prevention, transition added

--- small-works/3D-Dice/dice.js
+++ small-works/3D-Dice/dice.js
@@ -5,20 +5,27 @@ let xDegree = -30;
 let yDegree = 30;
 cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;  // initial transform values
 
+// setting transition duration
+const transitionDuration = 5000;
+cube.style.transition = transitionDuration / 1000 + 's'
+
+let lastClicked = Date.now() - transitionDuration;
+
 cube.addEventListener('click', function () {
-    [xDegree, yDegree] = randomRotation();
-    cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+    if (Date.now() - lastClicked > transitionDuration) {
+        [xDegree, yDegree] = randomRotation();
+        cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+        lastClicked = Date.now();
+    }
 });
 
 function randomRotation() {
     let xRot, yRot;
     do {
         xRot = (Math.floor(Math.random() * 48) + 12) * 90;
-        console.log(xRot - xDegree)
     } while (Math.abs(xRot - xDegree) <= 180);
     do {
         yRot = (Math.floor(Math.random() * 48) + 12) * 90;
-        console.log(yRot - yDegree)
     } while (Math.abs(yRot - yDegree) <= 180);
     return [xRot, yRot];
 }
