a277f2c7caede98dae9ba2a0dadabc1fdc61180e
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-23 00:30:54 +0330

    added click event with random rotation

--- small-works/3D-Dice/dice.js
+++ small-works/3D-Dice/dice.js
@@ -4,4 +4,21 @@ let cube = document.getElementsByClassName('cube')[0];
 let xDegree = -30;
 let yDegree = 30;
 cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;  // initial transform values
-console.log(cube.style.transform);
+
+cube.addEventListener('click', function () {
+    [xDegree, yDegree] = randomRotation();
+    cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+});
+
+function randomRotation() {
+    let xRot, yRot;
+    do {
+        xRot = (Math.floor(Math.random() * 48) + 12) * 90;
+        console.log(xRot - xDegree)
+    } while (Math.abs(xRot - xDegree) <= 180);
+    do {
+        yRot = (Math.floor(Math.random() * 48) + 12) * 90;
+        console.log(yRot - yDegree)
+    } while (Math.abs(yRot - yDegree) <= 180);
+    return [xRot, yRot];
+}
