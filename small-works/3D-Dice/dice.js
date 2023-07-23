61c9b65d42b165f27a64b251e8e23513ffc16efe
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-23 19:27:56 +0330

    made the code unfair so that the dice is fair. and more changes

--- small-works/3D-Dice/dice.js
+++ small-works/3D-Dice/dice.js
@@ -4,12 +4,14 @@ let container = document.getElementsByClassName('cube-container')[0];
 // degrees for x and y rotations
 let xDegree = -30;
 let yDegree = 30;
-cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;  // initial transform values
+let zDegree = 0;
+let resArr = [0, 0, 0, 0, 0, 0];
+cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;  // initial transform values
 
 // setting transition duration
 const transitionDuration = 5000;
 const transitionDurationOnKey = 500;
-cube.style.transition = transitionDuration / 1000 + 's ease-in-out';
+cube.style.transition = transitionDuration + 'ms ease-in-out';
 
 let touchStart = 0;
 let touchEnd = 0;
@@ -19,21 +21,24 @@ let lastClicked = Date.now() - transitionDuration;
 
 // click event
 cube.addEventListener('click', function () {
-    cube.style.transition = transitionDuration / 1000 + 's ease-in-out';
+    cube.style.transition = transitionDuration + 'ms ease-in-out';
     if (Date.now() - lastClicked > transitionDuration) {
-        [xDegree, yDegree] = randomRotation();
-        cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+        let result;
+        [xDegree, yDegree, zDegree, result] = randomRotation();
+        resArr[result - 1] += 1;
+        cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
+        console.log(resArr);
         lastClicked = Date.now();
         let resultElement = document.getElementsByClassName('result')[0];
         resultElement.style.transform = 'scale(0)';
-        resultElement.style.transition = (transitionDuration * 4 / 5000) + 's';
+        resultElement.style.transition = (transitionDuration * 4 / 5) + 'ms';
         resultElement.style.opacity = '0';
 
 
         function showResult() {
-            resultElement.innerHTML = findResult().toString();
+            resultElement.innerHTML = result.toString();
             resultElement.style.transform = 'scale(1)';
-            resultElement.style.transition = (transitionDuration / 5000) + 's';
+            resultElement.style.transition = (transitionDuration / 5) + 'ms';
             resultElement.style.opacity = '1';
         }
 
@@ -43,25 +48,34 @@ cube.addEventListener('click', function () {
 
 // handling arrow keys
 function keyPressed(key) {
-    cube.style.transition = transitionDurationOnKey / 1000 + 's ease-in-out';
+    cube.style.transition = transitionDurationOnKey + 'ms ease-in-out';
     switch (key) {
         case 'ArrowRight':
             yDegree += 15;
-            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
             break;
         case 'ArrowLeft':
             yDegree -= 15;
-            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
             break;
         case 'ArrowUp':
             xDegree += 15;
-            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
             break;
         case 'ArrowDown':
             xDegree -= 15;
-            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
+            break;
+        case '6':
+            zDegree += 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
+            break;
+        case '4':
+            zDegree -= 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg) rotateZ(${zDegree}deg)`;
             break;
     }
+    console.log(xDegree, yDegree, zDegree);
 }
 
 // keypress events
@@ -105,31 +119,166 @@ function swipeRotate() {
 }
 
 function randomRotation() {
-    let xRot, yRot;
-    do {
-        xRot = (Math.floor(Math.random() * 48) + 12) * 90;
-    } while (Math.abs(xRot - xDegree) <= 180);
-    do {
-        yRot = (Math.floor(Math.random() * 48) + 12) * 90;
-    } while (Math.abs(yRot - yDegree) <= 180);
-    return [xRot, yRot];
-}
-
-// finding the result based on rotation degrees
-function findResult() {
-    if (xDegree % 360 === 90) {
-        return 3;
-    } else if (xDegree % 360 === 270) {
-        return 4;
-    } else if ((xDegree + yDegree) % 360 === 0) {
-        return 1;
-    } else if ((xDegree + yDegree) % 360 === 90) {
-        return 5;
-    } else if ((xDegree + yDegree) % 360 === 180) {
-        return 6;
-    } else if ((xDegree + yDegree) % 360 === 270) {
-        return 2;
-    } else {
-        return 0;
+    function randomChoice(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
+
+    let randomDice = Math.floor(Math.random() * 6) + 1;
+    let xDeg, yDeg, zDeg;
+    switch (randomDice) {
+        case 1:
+            xDeg = yDeg = randomChoice([0, 180]);
+            zDeg = randomChoice([0, 90, 180, 270]);
+            break;
+        case 2:
+            xDeg = randomChoice([0, 90, 180, 270]);
+            switch (xDeg) {
+                case 0:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 180;
+                            break;
+                        case 270:
+                            zDeg = 0;
+                            break;
+                    }
+                    break;
+                case 90:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 90;
+                    break;
+                case 180:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 0;
+                            break;
+                        case 270:
+                            zDeg = 180;
+                            break;
+                    }
+                    break;
+                case 270:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 270;
+                    break;
+            }
+            break;
+        case 3:
+            xDeg = randomChoice([0, 90, 180, 270]);
+            switch (xDeg) {
+                case 0:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 90;
+                            break;
+                        case 270:
+                            zDeg = 270;
+                            break;
+                    }
+                    break;
+                case 90:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 0;
+                    break;
+                case 180:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 270;
+                            break;
+                        case 270:
+                            zDeg = 90;
+                            break;
+                    }
+                    break;
+                case 270:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 180;
+                    break;
+            }
+            break;
+        case 4:
+            xDeg = randomChoice([0, 90, 180, 270]);
+            switch (xDeg) {
+                case 0:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 270;
+                            break;
+                        case 270:
+                            zDeg = 90;
+                            break;
+                    }
+                    break;
+                case 90:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 180;
+                    break;
+                case 180:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 90;
+                            break;
+                        case 270:
+                            zDeg = 270;
+                            break;
+                    }
+                    break;
+                case 270:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 0;
+                    break;
+            }
+            break;
+        case 5:
+            xDeg = randomChoice([0, 90, 180, 270]);
+            switch (xDeg) {
+                case 0:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 0;
+                            break;
+                        case 270:
+                            zDeg = 180;
+                            break;
+                    }
+                    break;
+                case 90:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 270;
+                    break;
+                case 180:
+                    yDeg = randomChoice([90, 270]);
+                    switch (yDeg) {
+                        case 90:
+                            zDeg = 180;
+                            break;
+                        case 270:
+                            zDeg = 0;
+                            break;
+                    }
+                    break;
+                case 270:
+                    yDeg = randomChoice([0, 90, 180, 270]);
+                    zDeg = 90;
+                    break;
+            }
+            break;
+        case 6:
+            xDeg = randomChoice([0, 180]);
+            yDeg = Math.abs(xDeg - 180);
+            zDeg = randomChoice([0, 90, 180, 270]);
+            break;
+
+    }
+    xDeg += (Math.floor(Math.random() * 12) + 6) * 360;
+    yDeg += (Math.floor(Math.random() * 12) + 6) * 360;
+    zDeg += (Math.floor(Math.random() * 12) + 6) * 360;
+    return [xDeg, yDeg, zDeg, randomDice];
 }
