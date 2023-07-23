296b0f4e6e0103657e59fbe9cfaa8777b7d76d64
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-23 13:22:43 +0330

    added arrow keys functionality

--- small-works/3D-Dice/dice.js
+++ small-works/3D-Dice/dice.js
@@ -7,16 +7,50 @@ cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;  // init
 
 // setting transition duration
 const transitionDuration = 5000;
+const transitionDurationOnKey = 500;
 cube.style.transition = transitionDuration / 1000 + 's ease-in-out';
 
 let lastClicked = Date.now() - transitionDuration;
 
+// click event
 cube.addEventListener('click', function () {
+    cube.style.transition = transitionDuration / 1000 + 's ease-in-out';
     if (Date.now() - lastClicked > transitionDuration) {
         [xDegree, yDegree] = randomRotation();
         cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
         lastClicked = Date.now();
     }
+});
+
+// handling arrow keys
+function keyPressed(key) {
+    cube.style.transition = transitionDurationOnKey / 1000 + 's ease-in-out';
+    switch (key) {
+        case 'ArrowRight':
+            yDegree += 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            break;
+        case 'ArrowLeft':
+            yDegree -= 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            break;
+        case 'ArrowUp':
+            xDegree += 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            break;
+        case 'ArrowDown':
+            xDegree -= 15;
+            cube.style.transform = `rotateX(${xDegree}deg) rotateY(${yDegree}deg)`;
+            break;
+    }
+}
+
+// keypress events
+document.addEventListener('keydown', function (event) {
+    if (event.repeat || Date.now() - lastClicked <= transitionDuration) {
+        return;
+    }
+    keyPressed(event.key);
 });
 
 function randomRotation() {
