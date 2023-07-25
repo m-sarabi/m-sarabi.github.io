7b2aec9140e3cf3239b326f19e95aa61d6e9d73f
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-25 16:34:06 +0330

    added random array functions python style lol

--- small-works/match-elements/match-connect.js
+++ small-works/match-elements/match-connect.js
@@ -1,19 +1,29 @@
 let imgNames = ['apple', 'balloon', 'baseball-cap', 'coffee', 'donut', 'egg', 'fork', 'high-heels',
     'hot-drink', 'key', 'ladle', 'milk-bottle', 'muffin', 'orange', 'sneakers', 'umbrella'];
 
-function randomSample(arr, num) {
-    let slice = [];
-    let randomIndex;
-    num = Math.min(num, arr.length);
-    while (slice.length < num) {
-        randomIndex = Math.floor(Math.random() * arr.length);
-        slice.push(arr[randomIndex]);
-        arr.splice(randomIndex, 1);
+let imgCount = 6;
+
+const random = {
+    randomSample: function randomSample(arr, num) {
+        let slice = [];
+        let randomIndex;
+        num = Math.min(num, arr.length);
+        while (slice.length < num) {
+            randomIndex = Math.floor(Math.random() * arr.length);
+            slice.push(arr[randomIndex]);
+            arr.splice(randomIndex, 1);
+        }
+        return slice;
+    },
+
+    shuffleArray: function (arr) {
+        return arr.sort(function () {
+            return 0.5 - Math.random();
+        });
     }
-    return slice;
 }
 
-let randomImages = randomSample(imgNames, 16);
+let randomImages = random.randomSample(imgNames, imgCount * 2);
 let leftSide = document.querySelector('#left-list');
 let rightSide = document.querySelector('#right-list');
 
@@ -23,6 +33,7 @@ function addImages(image, parent) {
     imageElement.style.borderRadius = '10%';
     imageElement.style.padding = '2%';
     imageElement.style.boxShadow = '0 0 4px #000 inset';
+    imageElement.style.width = '100px';
     imageElement.setAttribute('src', '/assets/random-images/' + image + '.webp');
     parent.appendChild(imageElement);
     parent.appendChild(document.createElement('br'));
