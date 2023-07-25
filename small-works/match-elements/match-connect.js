df373da1187acd929914dbbacf1630631679f04f
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-25 18:49:01 +0330

    it is a mess, but got a few things to work.

--- small-works/match-elements/match-connect.js
+++ small-works/match-elements/match-connect.js
@@ -36,21 +36,80 @@ let randomImages = random.randomSample(imgNames, imgCount * 2);
 let leftSide = document.querySelector('#left-list');
 let rightSide = document.querySelector('#right-list');
 
+let randomCount1 = random.range(1, imgCount);
+let randomCount2 = random.range(1, imgCount);
+
+let leftContainers = [];
+for (let i = 0; i < imgCount; i++) {
+    let div = document.createElement('div');
+    let size = Math.floor(leftSide.clientWidth * 0.6);
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    div.style.textAlign = 'center';
+    div.style.position = 'relative';
+    div.style.border = '1px solid';
+    div.style.margin = '10px auto';
+    leftContainers.push(div);
+}
+
+leftContainers.forEach(function (div, index) {
+    let imgIndex = randomCount1[index];
+
+    let insideContainer = document.createElement('div');
+    insideContainer.style.position = 'absolute';
+    insideContainer.style.top = '50%';
+    insideContainer.style.width = 'inherit';
+    insideContainer.style.transform = 'translate(0, -50%)';
+
+    for (let i = 0; i < imgIndex; i++) {
+        addImages(randomImages[index], insideContainer);
+    }
+    div.appendChild(insideContainer);
+    leftSide.appendChild(div);
+});
+
+let rightContainer = [];
+for (let i = 0; i < imgCount; i++) {
+    let div = document.createElement('div');
+    let size = Math.floor(rightSide.clientWidth * 0.6);
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    div.style.textAlign = 'center';
+    div.style.position = 'relative';
+    div.style.border = '1px solid';
+    div.style.margin = '10px auto';
+    rightContainer.push(div);
+}
+
+rightContainer.forEach(function (div, index) {
+    let imgIndex = randomCount2[index];
+
+    let insideContainer = document.createElement('div');
+    insideContainer.style.position = 'absolute';
+    insideContainer.style.top = '50%';
+    insideContainer.style.width = 'inherit';
+    insideContainer.style.transform = 'translate(0, -50%)';
+
+    for (let i = 0; i < imgIndex; i++) {
+        addImages(randomImages[index + imgCount], insideContainer);
+    }
+    div.appendChild(insideContainer);
+    rightSide.appendChild(div);
+});
+
 function addImages(image, parent) {
     let imageElement = document.createElement('img');
     imageElement.style.backgroundColor = '#fffa';
     imageElement.style.borderRadius = '10%';
     imageElement.style.padding = '2%';
+    imageElement.style.margin = '1%';
     imageElement.style.boxShadow = '0 0 4px #000 inset';
-    imageElement.style.width = '100px';
+    imageElement.setAttribute('draggable', 'false');
+    imageElement.style.width = Math.floor((Math.random() / 2 + 0.5) * 64) + 'px';
     imageElement.setAttribute('src', '/assets/random-images/' + image + '.webp');
     parent.appendChild(imageElement);
-    parent.appendChild(document.createElement('br'));
 }
 
-randomImages.slice(0, randomImages.length / 2).forEach(function (image) {
-    addImages(image, leftSide);
-});
-randomImages.slice(randomImages.length / 2).forEach(function (image) {
-    addImages(image, rightSide);
-});
+// randomImages.slice(randomImages.length / 2).forEach(function (image) {
+//     addImages(image, rightSide);
+// });
