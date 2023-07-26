bc12521c794c7d97fce5e809ec92acb1d3c926a4
m-sarabi <m.sarabi.jkd@gmail.com>
2023-07-26 12:57:52 +0330

    improved the code by using nested loops and arrays

--- small-works/match-elements/match-connect.js
+++ small-works/match-elements/match-connect.js
@@ -1,8 +1,11 @@
+// a list of the small image file names
 let imgNames = ['apple', 'balloon', 'baseball-cap', 'coffee', 'donut', 'egg', 'fork', 'high-heels',
     'hot-drink', 'key', 'ladle', 'milk-bottle', 'muffin', 'orange', 'sneakers', 'umbrella'];
 
+// number of image groups on each side
 let imgCount = 6;
 
+// a few useful randomizing tools
 const random = {
     randomSample: function randomSample(arr, num) {
         let slice = [];
@@ -27,89 +30,70 @@ const random = {
         for (let i = start; i <= end; i++) {
             arr.push(i);
         }
-        arr = random.shuffleArray(arr);
+        arr = this.shuffleArray(arr);
         return arr;
     }
 };
 
+// randomized image names for both sides
 let randomImages = random.randomSample(imgNames, imgCount * 2);
-let leftSide = document.querySelector('#left-list');
-let rightSide = document.querySelector('#right-list');
 
-let randomCount1 = random.range(1, imgCount);
-let randomCount2 = random.range(1, imgCount);
+// an array of random images for each side
+let randomImageSide = [randomImages.slice(0, imgCount), randomImages.slice(imgCount)];
 
-let leftContainers = [];
-for (let i = 0; i < imgCount; i++) {
-    let div = document.createElement('div');
-    let size = Math.floor(leftSide.clientWidth * 0.6);
-    div.style.width = size + 'px';
-    div.style.height = size + 'px';
-    div.style.textAlign = 'center';
-    div.style.position = 'relative';
-    div.style.border = '1px solid';
-    div.style.margin = '10px auto';
-    leftContainers.push(div);
-}
+// each side container in an array
+let sideDivs = [];
+sideDivs[0] = document.querySelector('#left-list');
+sideDivs[1] = document.querySelector('#right-list');
 
-leftContainers.forEach(function (div, index) {
-    let imgIndex = randomCount1[index];
+// a random number for each image group
+let randomCount = [random.range(1, imgCount), random.range(1, imgCount)];
 
-    let insideContainer = document.createElement('div');
-    insideContainer.style.position = 'absolute';
-    insideContainer.style.top = '50%';
-    insideContainer.style.width = 'inherit';
-    insideContainer.style.transform = 'translate(0, -50%)';
+// populating each side
+sideDivs.forEach(function (sideDiv, sideIndex) {
+    // filling each container of the side with images
+    randomCount[sideIndex].forEach(function (imgGroup, imgIndex) {
+        // outside container of each image group
+        const outsideContainer = document.createElement('div');
+        let size = Math.floor(sideDiv.clientWidth * 0.6);
+        outsideContainer.style.width = size + 'px';
+        outsideContainer.style.height = size + 'px';
+        outsideContainer.style.textAlign = 'center';
+        outsideContainer.style.position = 'relative';
+        outsideContainer.style.border = '1px solid';
+        outsideContainer.style.margin = '10px auto';
+        console.log(typeof outsideContainer);
 
-    for (let i = 0; i < imgIndex; i++) {
-        addImages(randomImages[index], insideContainer);
-    }
-    div.appendChild(insideContainer);
-    leftSide.appendChild(div);
+        // inside container of each image group
+        const insideContainer = document.createElement('div');
+        insideContainer.style.position = 'absolute';
+        insideContainer.style.top = '50%';
+        insideContainer.style.width = 'inherit';
+        insideContainer.style.transform = 'translate(0, -50%)';
+        // adding images to each group
+        for (let i = 0; i < randomCount[sideIndex][imgIndex]; i++) {
+            addImages(randomImageSide[sideIndex][imgIndex], insideContainer);
+        }
+        // putting containers inside each other
+        outsideContainer.appendChild(insideContainer);
+        sideDiv.appendChild(outsideContainer);
+    });
 });
 
-let rightContainer = [];
-for (let i = 0; i < imgCount; i++) {
-    let div = document.createElement('div');
-    let size = Math.floor(rightSide.clientWidth * 0.6);
-    div.style.width = size + 'px';
-    div.style.height = size + 'px';
-    div.style.textAlign = 'center';
-    div.style.position = 'relative';
-    div.style.border = '1px solid';
-    div.style.margin = '10px auto';
-    rightContainer.push(div);
-}
-
-rightContainer.forEach(function (div, index) {
-    let imgIndex = randomCount2[index];
-
-    let insideContainer = document.createElement('div');
-    insideContainer.style.position = 'absolute';
-    insideContainer.style.top = '50%';
-    insideContainer.style.width = 'inherit';
-    insideContainer.style.transform = 'translate(0, -50%)';
-
-    for (let i = 0; i < imgIndex; i++) {
-        addImages(randomImages[index + imgCount], insideContainer);
-    }
-    div.appendChild(insideContainer);
-    rightSide.appendChild(div);
-});
-
+/**
+ * adding an image to a parent container
+ * @param image {string}     name of the image file, should have .webp format
+ * @param parent {HTMLDivElement}    the parent container that image is added to
+ */
 function addImages(image, parent) {
     let imageElement = document.createElement('img');
     imageElement.style.backgroundColor = '#fffa';
     imageElement.style.borderRadius = '10%';
     imageElement.style.padding = '2%';
     imageElement.style.margin = '1%';
-    imageElement.style.boxShadow = '0 0 4px #000 inset';
+    imageElement.style.boxShadow = '0 0 10px #0007 inset';
     imageElement.setAttribute('draggable', 'false');
     imageElement.style.width = Math.floor((Math.random() / 2 + 0.5) * 64) + 'px';
     imageElement.setAttribute('src', '/assets/random-images/' + image + '.webp');
     parent.appendChild(imageElement);
 }
-
-// randomImages.slice(randomImages.length / 2).forEach(function (image) {
-//     addImages(image, rightSide);
-// });
