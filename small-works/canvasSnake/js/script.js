9bb2c04f458811fd86471cf41f5cf292142e97c6
m-sarabi <m.sarabi.jkd@gmail.com>
2023-08-13 00:00:10 +0330

    canvasSnake added

---
+++ small-works/canvasSnake/js/script.js
@@ -0,0 +1,146 @@
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 900;
+canvas.style.backgroundColor = '#90ee90';
+let cellSize = 60;
+
+canvas.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    return false;
+}, false);
+
+function drawGrid() {
+    ctx.beginPath();
+    ctx.setLineDash([10, 2]);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#0005';
+    for (let i = cellSize; i < canvas.height; i += cellSize) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+    }
+    for (let i = cellSize; i < canvas.width; i += cellSize) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+    }
+    ctx.stroke();
+    ctx.setLineDash([]);
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 5;
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    ctx.closePath();
+}
+
+
+let x = 0, y = 0;
+let dx = 0.5, dy = 0.5;
+let ddx = 0, ddy = 0;
+let direction = null;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x + cellSize / 2, y + cellSize / 2, cellSize / 3, 0, Math.PI * 2);
+    ctx.fillStyle = 'darkred';
+    ctx.fill();
+    ctx.closePath();
+
+    if (x % cellSize === 0 && y % cellSize === 0) {
+        switch (direction) {
+            case 'right':
+                ddx = dx;
+                ddy = 0;
+                break;
+            case 'down':
+                ddx = 0;
+                ddy = dy;
+                break;
+            case 'left':
+                ddx = -dx;
+                ddy = 0;
+                break;
+            case 'up':
+                ddx = 0;
+                ddy = -dy;
+                break;
+        }
+    }
+    if (x >= 0 && x <= canvas.width - cellSize) {
+        x += ddx;
+        x = Math.min(canvas.width - cellSize, Math.max(0, x));
+    }
+    if (y >= 0 && y <= canvas.height - cellSize) {
+        y += ddy;
+        y = Math.min(canvas.height - cellSize, Math.max(0, y));
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
+    drawBall();
+}
+
+setInterval(draw, 1);
+
+function directionKey(key) {
+    switch (key) {
+        case 'ArrowRight':
+        case 'KeyD':
+            direction = 'right';
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            direction = 'down';
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            direction = 'left';
+            break;
+        case 'ArrowUp':
+        case 'KeyW':
+            direction = 'up';
+            break;
+    }
+}
+
+window.addEventListener('keydown', function (event) {
+    let key = event.code;
+    directionKey(key);
+});
+
+let touchPos = [[], []];
+canvas.addEventListener('touchstart', function (event) {
+    event.preventDefault();
+    touchPos[0] = [event.changedTouches[0].screenX, event.changedTouches[0].screenY];
+});
+
+window.addEventListener('touchmove', function (event) {
+    touchPos[1] = [event.changedTouches[0].screenX, event.changedTouches[0].screenY];
+    touchToKey();
+});
+
+window.addEventListener('touchend', function () {
+    touchPos = [[], []];
+});
+
+function touchToKey() {
+    let touchThreshold = cellSize * 0.75;
+    if (Math.abs(touchPos[1][0] - touchPos[0][0]) > Math.abs(touchPos[1][1] - touchPos[0][1])) {
+        if (touchPos[1][0] - touchPos[0][0] > touchThreshold) {
+            directionKey('ArrowRight');
+            touchPos[0] = touchPos[1];
+        } else if (touchPos[1][0] - touchPos[0][0] < -touchThreshold) {
+            directionKey('ArrowLeft');
+            touchPos[0] = touchPos[1];
+        }
+    } else if (Math.abs(touchPos[1][0] - touchPos[0][0]) < Math.abs(touchPos[1][1] - touchPos[0][1])) {
+        if (touchPos[1][1] - touchPos[0][1] > touchThreshold) {
+            directionKey('ArrowDown');
+            touchPos[0] = touchPos[1];
+        } else if (touchPos[1][1] - touchPos[0][1] < -touchThreshold) {
+            directionKey('ArrowUp');
+            touchPos[0] = touchPos[1];
+
+        }
+    }
+}
