ea9a33270c380a2c7155756b452c2e2724a61fe0
m-sarabi <m.sarabi.jkd@gmail.com>
2023-10-12 15:03:50 +0330

    week 2 ToC and a fun script

---
+++ python/js/fun.js
@@ -0,0 +1,15 @@
+funElem = document.getElementById("fun")
+let fullText = "Happy coding, and good luck!       "
+let text = fullText
+funElem.innerHTML = ""
+let counter = 0
+let funInterval = setInterval(function () {
+    funElem.innerHTML += text.charAt(0)
+    text = text.substring(1)
+    counter += 1
+    if (text === "") {
+        funElem.innerHTML = ""
+        text = fullText
+        // clearInterval(funInterval)
+    }
+}, 600)
