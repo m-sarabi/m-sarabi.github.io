47b15e83932e02b250167bdab9bd5991ec084ffe
m-sarabi <m.sarabi.jkd@gmail.com>
2023-10-19 13:11:28 +0330

    ignoring the animated text when it is not included

--- python/js/script.js
+++ python/js/script.js
@@ -2,19 +2,20 @@
 funElem = document.getElementById("fun");
 let fullText = "Happy coding, and good luck!       ";
 let text = fullText;
-funElem.innerHTML = " ";
-let counter = 0;
-let funInterval = setInterval(function () {
-    funElem.innerHTML += text.charAt(0);
-    text = text.substring(1);
-    counter += 1;
-    if (text === "") {
-        funElem.innerHTML = " ";
-        text = fullText;
-        // clearInterval(funInterval)
-    }
-}, 600);
-
+if (funElem !== null) {
+    funElem.innerHTML = " ";
+    let counter = 0;
+    let funInterval = setInterval(function () {
+        funElem.innerHTML += text.charAt(0);
+        text = text.substring(1);
+        counter += 1;
+        if (text === "") {
+            funElem.innerHTML = " ";
+            text = fullText;
+            // clearInterval(funInterval)
+        }
+    }, 600);
+}
 // toc generator
 const headElements = document.querySelectorAll("h2");
 if (document.getElementsByClassName("toc")[0] !== undefined) {
