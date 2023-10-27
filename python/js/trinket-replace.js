77d95be84cd3ec59e5b246ba49f13de57077710b
m-sarabi <m.sarabi.jkd@gmail.com>
2023-10-27 23:52:06 +0330

    not displaying the trinket when printing

--- python/js/trinket-replace.js
+++ python/js/trinket-replace.js
@@ -1,6 +1,7 @@
 fetch("./trinket.html").then(result => result.text()).then(text => {
     let old_elem = document.querySelector("script#trinket-script");
     let new_elem = document.createElement("div");
+    new_elem.classList.add('trinket')
     new_elem.innerHTML = text;
     old_elem.parentNode.replaceChild(new_elem, old_elem);
 })
