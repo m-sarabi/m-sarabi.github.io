b398cdafb722373946b478e5748826cb5222798c
m-sarabi <m.sarabi.jkd@gmail.com>
2023-11-28 21:15:21 +0330

    fixed page titles

--- python/js/inject.js
+++ python/js/inject.js
@@ -16,27 +16,41 @@ function GetUrlRelativePath() {
     return relUrl;
 }
 
+function pageTitle(title) {
+    let name;
+    if (title !== null) {
+        name = title
+    } else {
+        name = pageUrl.substring(pageUrl.lastIndexOf('/') + 1, pageUrl.lastIndexOf('.'))
+    }
+    return name[0].toUpperCase() + name.substring(1).toLowerCase() + " - Python/MSarabi"
+}
+
 pageUrl = GetUrlRelativePath()
 
 // get the content of the current document excluding this script
-fetch(pageUrl).then(result => result.text()).then(text => {
-    text = text.substring(0, text.lastIndexOf('\n'))
-
+fetch(pageUrl).then(result => result.text()).then(content => {
+    content = content.substring(0, content.lastIndexOf('\n'))
+    let properties = null, title
+    if (content.startsWith("{")) {
+        properties = JSON.parse(content.substring(0, content.indexOf('}') + 1))
+        title = pageTitle(properties["title"])
+        content = content.substring(content.indexOf("}") + 2)
+    }
     // URL of the template file
     let templateUrl = pageUrl.substring(0, pageUrl.lastIndexOf('/')) + "/template.html";
-    console.log(templateUrl)
 
     // inject this content into the template file and write it again to this document
     fetch(templateUrl).then(result => result.text()).then(textTemp => {
-        textTemp = textTemp.replace("{{ title }}", "Test Title")
-        textTemp = textTemp.replace("{{ content }}", text)
+        textTemp = textTemp.replace("{{ title }}", title)
+        textTemp = textTemp.replace("{{ content }}", content)
         document.open()
         document.write(textTemp)
         document.close()
 
         const h2Elements = document.querySelectorAll("h2")
         if (h2Elements.length <= 1) {
-            document.getElementById("tkinter-container").remove()
+            document.getElementById("trinket-container").remove()
             document.querySelector(".toc").remove()
         }
 
