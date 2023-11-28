f9eccc4b39ce5662564c903224543f6fb60f9e9d
m-sarabi <m.sarabi.jkd@gmail.com>
2023-11-28 15:11:06 +0330

    reusable static template.html

--- python/js/inject.js
+++ python/js/inject.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the relative URL path of the current document.
+ *
+ * @return {string} relative URL path
+ */
 function GetUrlRelativePath() {
     const url = document.location.toString();
     const arrUrl = url.split("//");
@@ -12,18 +17,28 @@ function GetUrlRelativePath() {
 }
 
 pageUrl = GetUrlRelativePath()
-console.log(pageUrl)
 
+// get the content of the current document excluding this script
 fetch(pageUrl).then(result => result.text()).then(text => {
     text = text.substring(0, text.lastIndexOf('\n'))
-    console.log(text)
-})
 
-let templateUrl = pageUrl.substring(0, pageUrl.lastIndexOf('/')) + "/template.html";
-console.log(templateUrl)
+    // URL of the template file
+    let templateUrl = pageUrl.substring(0, pageUrl.lastIndexOf('/')) + "/template.html";
+    console.log(templateUrl)
 
-fetch(templateUrl).then(result => result.text()).then(text => {
-    document.open()
-    document.write(text)
-    document.close()
+    // inject this content into the template file and write it again to this document
+    fetch(templateUrl).then(result => result.text()).then(textTemp => {
+        textTemp = textTemp.replace("{{ title }}", "Test Title")
+        textTemp = textTemp.replace("{{ content }}", text)
+        document.open()
+        document.write(textTemp)
+        document.close()
+
+        const h2Elements = document.querySelectorAll("h2")
+        if (h2Elements.length <= 1) {
+            document.getElementById("tkinter-container").remove()
+            document.querySelector(".toc").remove()
+        }
+
+    })
 })
