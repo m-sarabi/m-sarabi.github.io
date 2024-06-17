ce78447479a4f0a36e99373572544c6dc58d3e3d
m-sarabi <m.sarabi.jkd@gmail.com>
2024-06-17 20:43:19 +0330

    Now with memory

--- small-works/programResultCreator/script.js
+++ small-works/programResultCreator/script.js
@@ -1,5 +1,3 @@
-// When add button is pressed, create a new card div inside "result" div, and add title, goal, and progress values to it
-
 const svgFail = $("<svg clip-rule=\"evenodd\" fill-rule=\"evenodd\" stroke-linejoin=\"round\" stroke-miterlimit=\"2\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 1.5c-4.69 0-8.497 3.807-8.497 8.497s3.807 8.498 8.497 8.498 8.498-3.808 8.498-8.498-3.808-8.497-8.498-8.497zm0 7.425 2.717-2.718c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.717 2.717 2.727 2.728c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.384-.073-.53-.219l-2.729-2.728-2.728 2.728c-.146.146-.338.219-.53.219-.401 0-.751-.323-.751-.75 0-.192.073-.384.22-.531l2.728-2.728-2.722-2.722c-.146-.147-.219-.338-.219-.531 0-.425.346-.749.75-.749.192 0 .385.073.531.219z\" fill-rule=\"nonzero\"/></svg>")
 svgFail.attr("width", 40)
 svgFail.attr("fill", "red")
@@ -15,107 +13,151 @@ svgCheck.attr("fill", "green")
 const svgGold = svgCheck.clone()
 svgGold.attr("fill", "gold")
 
-// click event for "add" button
-const addBtn = $("#add");
-addBtn.click(function () {
-    const title = $("#title");
-    const goal = $("#goal");
-    const progress = $("#progress");
-    const result = $("#result");
+let titles = []
+let goals = []
+let progresses = []
 
-    // check if title, goal, and progress inputs are empty
-    if (title.val() === "" || goal.val() === "" || progress.val() === "") {
-        alert("Please fill in all fields");
-        return;
+$(document).ready(function () {
+    function createCard(title, goal, progress) {
+        // create a div with class .item-container
+        const cardDiv = $("<div>").addClass("card")
+        const valuesDiv = $("<div>").addClass("item-container");
+        const result = $("#result");
+        result.append(valuesDiv);
+
+        valuesDiv.append(`<span class="c-title item">${title}</span>`)
+        valuesDiv.append(`<span class="c-goal item">${goal}</span>`)
+        valuesDiv.append(`<span class="c-progress item">${progress}</span>`)
+
+        const progressPercentage = Math.round(Math.max(0, Math.min(progress, goal)) / goal * 100)
+
+        // add a progress bar
+        const progressContainer = $("<div>").addClass("progress-container")
+        const progressLabel = $("<span class='progress-label'>").text(progressPercentage + "%")
+
+        // recreate progress bar using divs
+        const progressBarUnfilled = $("<div>").addClass("progress-bar unfilled")
+        const progressBarFilled = $("<div>").addClass("progress-bar filled")
+
+        progressBarFilled.css("width", progressPercentage + "%")
+        progressBarUnfilled.append(progressBarFilled)
+
+        cardDiv.append(valuesDiv);
+        progressContainer.append(progressLabel)
+        progressContainer.append(progressBarUnfilled)
+
+        if (progressPercentage >= 100) {
+            progressContainer.append(svgGold.clone())
+        } else if (progressPercentage >= 80) {
+            progressContainer.append(svgCheck.clone())
+        } else if (progressPercentage >= 50) {
+            progressContainer.append(svgAlert.clone())
+        } else {
+            progressContainer.append(svgFail.clone())
+        }
+
+        cardDiv.append(progressContainer)
+
+        // if number of .card is bigger than one, add a horizontal line
+        if (result.children(".card").length > 1) {
+            result.append("<hr>")
+        }
+
+        result.append(cardDiv);
     }
 
-    // create a div with class .item-container
-    const cardDiv = $("<div>").addClass("card")
-    const valuesDiv = $("<div>").addClass("item-container");
-    result.append(valuesDiv);
-
-    valuesDiv.append(`<span class="c-title item">${title.val()}</span>`)
-    valuesDiv.append(`<span class="c-goal item">${goal.val()}</span>`)
-    valuesDiv.append(`<span class="c-progress item">${progress.val()}</span>`)
-
-    const progressPercentage = Math.round(Math.max(0, Math.min(progress.val(), goal.val())) / goal.val() * 100)
-
-    // add a progress bar
-    const progressContainer = $("<div>").addClass("progress-container")
-    // const progressBar = $("<progress>")
-    // progressBar.attr("max", goal.val())
-    // progressBar.attr("value", Math.max(0, Math.min(progress.val(), goal.val())))
-    const progressLabel = $("<span class='progress-label'>").text(progressPercentage + "%")
-
-    // recreate progress bar using divs
-    const progressBarUnfilled = $("<div>").addClass("progress-bar unfilled")
-    const progressBarFilled = $("<div>").addClass("progress-bar filled")
-
-    progressBarFilled.css("width", progressPercentage + "%")
-    progressBarUnfilled.append(progressBarFilled)
-
-    cardDiv.append(valuesDiv);
-    progressContainer.append(progressLabel)
-    progressContainer.append(progressBarUnfilled)
-
-    if (progressPercentage >= 100) {
-        progressContainer.append(svgGold.clone())
-    } else if (progressPercentage >= 80) {
-        progressContainer.append(svgCheck.clone())
-    } else if (progressPercentage >= 50) {
-        progressContainer.append(svgAlert.clone())
-    } else {
-        progressContainer.append(svgFail.clone())
+    // save an array to cookies
+    function saveArrayToCookie(name, array) {
+        $.cookie(name, JSON.stringify(array), {expires: 30})
     }
 
-    cardDiv.append(progressContainer)
-
-    // if number of .card is bigger than one, add a horizontal line
-    if (result.children(".card").length > 1) {
-        result.append("<hr>")
+    // get an array from cookies
+    function getArrayFromCookie(name) {
+        return JSON.parse($.cookie(name))
     }
 
-    result.append(cardDiv);
-    // clear title, goal, and progress inputs
-    title.val("");
-    goal.val("");
-    progress.val("");
-})
+    // if cookies are not empty, load them
+    if ($.cookie("titles") != null && getArrayFromCookie("titles") != null) {
+        titles = getArrayFromCookie("titles")
+        goals = getArrayFromCookie("goals")
+        progresses = getArrayFromCookie("progresses")
+        for (let i = 0; i < titles.length; i++) {
+            createCard(titles[i], goals[i], progresses[i])
+        }
+    }
 
-// click event for "clear" button, to clear all the cards, except the first one
-const clearBtn = $("#clear");
-clearBtn.click(function () {
-    const result = $("#result");
-    result.children(".card").each(function (index) {
-        if (index > 0) {
-            $(this).remove();
+    // click event for "add" button
+    const addBtn = $("#add")
+    addBtn.click(function () {
+        const title = $("#title");
+        const goal = $("#goal");
+        const progress = $("#progress");
+
+        // check if title, goal, and progress inputs are empty
+        if (title.val() === "" || goal.val() === "" || progress.val() === "") {
+            alert("Please fill in all fields");
+            return;
+        }
+
+        // save the title, goal and progress in cookies
+        titles.push(title.val())
+        goals.push(goal.val())
+        progresses.push(progress.val())
+        saveArrayToCookie("titles", titles)
+        saveArrayToCookie("goals", goals)
+        saveArrayToCookie("progresses", progresses)
+
+        // create a card
+        createCard(title.val(), goal.val(), progress.val());
+
+        // clear title, goal, and progress inputs
+        title.val("");
+        goal.val("");
+        progress.val("");
+    })
+
+    // click event for "clear" button, to clear all the cards, except the first one
+    const clearBtn = $("#clear");
+    clearBtn.click(function () {
+        const result = $("#result");
+        result.children(".card").each(function (index) {
+            if (index > 0) {
+                $(this).remove();
+            }
+        })
+        // remove all <hr> elements
+        result.children("hr").remove();
+
+        // remove all cookies
+        $.removeCookie("titles", {expires: 30})
+        $.removeCookie("goals", {expires: 30})
+        $.removeCookie("progresses", {expires: 30})
+        titles = []
+        goals = []
+        progresses = []
+    })
+
+    // click event for "download" button, to download the result as an image
+    const downloadBtn = $("#download");
+    downloadBtn.click(function () {
+        const result = document.getElementById("result");
+        html2canvas(result).then(function (canvas) {
+            // $("#preview").append(canvas)
+            const link = document.createElement("a")
+            link.href = canvas.toDataURL("image/png");
+            link.download = "result.png";
+            document.body.appendChild(link);
+            link.click();
+        })
+    })
+
+    // assign enter key to the add button, if on #progress input
+    // and move the focus to the #title input
+    const progressInput = $("#progress");
+    progressInput.on("keyup", function (event) {
+        if (event.key === "Enter") {
+            addBtn.click();
+            $("#title").focus();
         }
     })
-    // remove all <hr> elements
-    result.children("hr").remove();
-})
-
-
-// click event for "download" button, to download the result as an image
-const downloadBtn = $("#download");
-downloadBtn.click(function () {
-    const result = document.getElementById("result");
-    html2canvas(result).then(function (canvas) {
-        // $("#preview").append(canvas)
-        const link = document.createElement("a")
-        link.href = canvas.toDataURL("image/png");
-        link.download = "result.png";
-        document.body.appendChild(link);
-        link.click();
-    })
-})
-
-// assign enter key to the add button, if on #progress input
-// and move the focus to the #title input
-const progressInput = $("#progress");
-progressInput.on("keyup", function (event) {
-    if (event.key === "Enter") {
-        addBtn.click();
-        $("#title").focus();
-    }
 })
