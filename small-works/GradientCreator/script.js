efb91e1e9c883d8d3b3dde437ffa5068004c63ad
m-sarabi <m.sarabi.jkd@gmail.com>
2024-07-16 12:05:17 +0330

    updated with color gradient generator

--- small-works/GradientCreator/script.js
+++ small-works/GradientCreator/script.js
@@ -1,6 +1,37 @@
 let color1;
 let color2;
 
+class gradientColor {
+    constructor(color) {
+        this.color = color;
+    }
+
+    createContainer() {
+        const container = document.createElement('div');
+        container.style.backgroundColor = this.color;
+        container.classList.add('gradient-color');
+
+        container.appendChild(this.createCopyButton());
+
+        container.addEventListener('click', () => {
+            const rgb = container.style.backgroundColor;
+            const hex = rgbToHex(rgb);
+            navigator.clipboard.writeText(hex).then();
+        });
+
+        return container;
+    }
+
+    createCopyButton() {
+        const button = document.createElement('div');
+        button.classList.add('select-color');
+        const text = document.createElement('span');
+        text.innerHTML = 'copy';
+        button.appendChild(text);
+        return button;
+    }
+}
+
 // convert rgb (as "rgb(255, 255, 255)") to hex (#ffffff)
 function rgbToHex(rgb) {
     rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
@@ -16,9 +47,7 @@ function rgbToHex(rgb) {
 function hexToRgb(hex) {
     let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
+        r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16)
     } : null;
 }
 
@@ -36,7 +65,6 @@ function calculateGradient(color1, color2, gradientSize) {
     let rStep = (r2 - r1) / (gradientSize - 1);
     let gStep = (g2 - g1) / (gradientSize - 1);
     let bStep = (b2 - b1) / (gradientSize - 1);
-    console.log(gradientSize);
     for (let i = 0; i < gradientSize; i++) {
         let r = Math.round(r1 + i * rStep);
         let g = Math.round(g1 + i * gStep);
@@ -46,54 +74,70 @@ function calculateGradient(color1, color2, gradientSize) {
     return gradient;
 }
 
-$(document).ready(function () {
-    color1 = $("#color1-input").val();
-    color2 = $("#color2-input").val();
+document.addEventListener('DOMContentLoaded', async function () {
+    color1 = document.getElementById("color1-input").value;
+    color2 = document.getElementById("color2-input").value;
 
-    const gradientSizeInput = $("#gradient-size");
-    let gradientSize = gradientSizeInput.val();
-    gradientSizeInput.on("input", function () {
-        $("#gradient-size-value").html($(this).val());
-        gradientSize = $(this).val();
+    const gradientSizeInput = document.getElementById("gradient-size");
+    const gradientSizeValue = document.getElementById("gradient-size-value");
+    gradientSizeInput.addEventListener("input", function () {
+        gradientSizeValue.value = this.value;
         createGradient();
     });
 
-    $("#color1-color").css("background-color", color1);
-    $("#color2-color").css("background-color", color2);
-
-    $(".select-color").on("click", function () {
-        const rgb = $(this).css("background-color");
-        const hex = rgbToHex(rgb);
-        navigator.clipboard.writeText(hex).then();
-    });
-
-    // change background color of .select-color on input change
-    $("input[type=color]").on("input", function () {
-        $(this).parent().parent().children(".select-color").css("background-color", $(this).val());
-        if (this.id === "color1-input") {
-            color1 = $(this).val();
-        } else {
-            color2 = $(this).val();
+    gradientSizeValue.addEventListener("input", function () {
+        if (this.value > 20) {
+            this.value = 20;
+        } else if (this.value < 2) {
+            this.value = 2;
         }
+        gradientSizeInput.value = this.value;
         createGradient();
+    });
+
+    document.getElementById("color1-color").style.backgroundColor = color1;
+    document.getElementById("color2-color").style.backgroundColor = color2;
+
+    document.querySelectorAll('.select-color').forEach(function (el) {
+        el.addEventListener('click', function () {
+            const rgb = this.style.backgroundColor;
+            const hex = rgbToHex(rgb);
+            navigator.clipboard.writeText(hex).then();
+        });
+    });
+
+    document.querySelectorAll('input[type=color]').forEach(function (el) {
+        el.addEventListener('input', function () {
+            const parent = this.parentNode.parentNode;
+            parent.querySelector('.select-color').style.backgroundColor = this.value;
+
+            if (this.id === "color1-input") {
+                color1 = this.value;
+            } else {
+                color2 = this.value;
+            }
+            createGradient();
+        });
     });
 
     function createGradient() {
         let gradientColors;
         const gradientDivs = [];
-        gradientColors = calculateGradient(hexToRgb(color1), hexToRgb(color2), gradientSize);
+        gradientColors = calculateGradient(hexToRgb(color1), hexToRgb(color2), gradientSizeInput.value);
 
         gradientColors.forEach((color) => {
-            gradientDivs.push(`<div style="background-color: ${color}" class="gradient-color"></div>`);
+            const gradientDiv = new gradientColor(color).createContainer();
+            gradientDivs.push(gradientDiv);
         });
-        $("#gradient").html("").html(gradientDivs.join(" "));
+        document.getElementById('gradient').innerHTML = "";
+        document.getElementById('gradient').append(...gradientDivs);
     }
 
     createGradient();
 
-    $(document).on("click", ".gradient-color", function () {
-        const rgb = $(this).css("background-color");
-        const hex = rgbToHex(rgb);
-        navigator.clipboard.writeText(hex).then();
-    });
+    // $(document).on("click", ".gradient-color", function () {
+    //     const rgb = $(this).css("background-color");
+    //     const hex = rgbToHex(rgb);
+    //     navigator.clipboard.writeText(hex).then();
+    // });
 });
